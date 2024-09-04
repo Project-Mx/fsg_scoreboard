2753b92390d3a4fdc8e290f449f439292e1b6032
Project-Mx <98364167+Project-Mx@users.noreply.github.com>
2024-09-04 07:28:29 -0400

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -2,26 +2,31 @@ lib.callback.register('fsg_scoreboard:getPlayers', function(source)
     local data = {}
     local players = GetPlayers()
     for k, v in ipairs(players) do
-        local playerId = QBCore and GetPlayer(v).PlayerData.source or v.source
-        local playerName
-        if Config.UseSteamHexAsPlayerName then
-            playerName = GetPlayerIdentifierByType(tostring(playerId), 'steam')
-        else
-            playerName = GetPlayerName(playerId)
-        end
-        table.insert(data, {playerId = playerId, playerName = ''..playerName})
+        local player = ESX.GetPlayerFromId(v)
+        local playerId = player and player.source or v.source
+        local playerName = GetPlayerName(playerId) or 'Unknown'
+        local steamIdentifier = GetPlayerIdentifierByType(tostring(playerId), 'steam') or 'Unknown'
+        local playerGroup = player and player.getGroup() or 'Unknown' 
+        table.insert(data, {playerId = playerId, playerName = playerName , steamIdentifier = steamIdentifier})
     end
     return data
 end)
 
 lib.callback.register('fsg_scoreboard:getSpecificPlayer', function(source, target)
     local identifiers = {}
-    local player = GetPlayer(target)
-    local playerId = QBCore and player.PlayerData.source or player.source
-    local fivemid = GetPlayerIdentifierByType(tostring(playerId), 'fivem')
-    local discordid = GetPlayerIdentifierByType(tostring(playerId), 'discord')
-    local xblid = GetPlayerIdentifierByType(tostring(playerId), 'xbl')
-    local liveid = GetPlayerIdentifierByType(tostring(playerId), 'liveid')
+    local player = ESX.GetPlayerFromId(target)
+    local playerId = player and player.source or target.source
+
+    local fivemid = GetPlayerIdentifierByType(tostring(playerId), 'fivem') or 'Unknown'
+    local discordid = GetPlayerIdentifierByType(tostring(playerId), 'discord') or 'Unknown'
+    local xblid = GetPlayerIdentifierByType(tostring(playerId), 'xbl') or 'Unknown'
+    local liveid = GetPlayerIdentifierByType(tostring(playerId), 'liveid') or 'Unknown'
+
     table.insert(identifiers, {fivemid, discordid, xblid, liveid})
-    return playerId, GetPlayerName(playerId), GetPlayerIdentifierByType(tostring(playerId), 'steam'), identifiers
+
+    local playerName = GetPlayerName(playerId) or 'Unknown'
+    local steamIdentifier = GetPlayerIdentifierByType(tostring(playerId), 'steam') or 'Unknown'
+    local playerGroup = player and player.getGroup() or 'Unknown' 
+
+    return playerId, playerName .. ' [' .. playerGroup .. ']', steamIdentifier, identifiers
 end)
