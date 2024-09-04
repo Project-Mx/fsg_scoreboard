848f5b116116c483995bb3703ae47d43d15141d7
Project-Mx <98364167+Project-Mx@users.noreply.github.com>
2024-09-04 07:27:36 -0400

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,16 +1,7 @@
 RegisterCommand(Config.Command, function(source)
-    lib.registerMenu({
-        id = 'fsg_scoreboard_main',
-        title = locale('scoreboard_title'),
-        position = 'top-right',
-        options = {
-            { label = 'View Online Players', icon = 'fa-solid fa-users', close = true }
-        }
-    }, function(selected, scrollIndex, args)
-        showScoreboard()
-    end)
-    lib.showMenu('fsg_scoreboard_main')
+    showScoreboard()  
 end)
+
 if Config.Keybind then
     RegisterKeyMapping(Config.Command, 'Open Scoreboard', 'KEYBOARD', Config.Keybind)
 end
