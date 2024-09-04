2ceda27bfce17ad94e7035ac1597106a4b2ff0d9
Project-Mx <98364167+Project-Mx@users.noreply.github.com>
2024-09-04 07:28:13 -0400

    Update functions.lua

--- server/functions.lua
+++ server/functions.lua
@@ -8,7 +8,7 @@ function GetJob(source)
   elseif Ox then
     local player = Ox.getPlayer(source)
 
-    return player.getGroup() -- Haven't used OX Core too much before, seeing from documentation that police and sheriff were stored as a group so that's what I'll assume is the job.
+    return player.getGroup()
   end
 end
 
